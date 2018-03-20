891fabfc3660b1ba628f8e4243494220550273b1
Nick Stokoe <nick.stokoe@controllis.com>
2018-03-20 12:50:36 +0000

    demo case

---
+++ src/main/groovy/foo/Test.groovy
@@ -0,0 +1,7 @@
+package foo
+
+public class Test {
+    public static void main(String[] args) {
+        println "groovy world"
+    }
+}
---
+++ src/main/groovy/foo/Test2.java
@@ -0,0 +1,7 @@
+package foo;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("java world");
+    }
+}
